905b29b5d337a540089cad6c1c818b416d12d079
SeungTae <dltmdxo1589@daum.net>
2024-02-22 23:49:51 +0900

    Basic Page

--- src/main/java/seungtae/first/controller/MappingController.java
+++ src/main/java/seungtae/first/controller/MappingController.java
@@ -1,11 +1,13 @@
 package seungtae.first.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/page")
+@RequiredArgsConstructor
 public class MappingController {
 
     @GetMapping("/login")
@@ -16,5 +18,10 @@ public class MappingController {
     @GetMapping("/contact")
     public String contact() {
         return "page/contact";
+    }
+
+    @GetMapping("/signup")
+    public String signup() {
+        return "page/signup";
     }
 }
