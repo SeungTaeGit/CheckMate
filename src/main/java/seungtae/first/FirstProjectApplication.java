0f3f2951e6a49f23d53ad79a819af53714aa553c
SeungTae <dltmdxo1589@daum.net>
2024-02-22 22:30:39 +0900

    Create project

---
+++ src/main/java/seungtae/first/FirstProjectApplication.java
@@ -0,0 +1,13 @@
+package seungtae.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectApplication.class, args);
+	}
+
+}
