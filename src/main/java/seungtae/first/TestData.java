944807885368e9969aca93e74eec45a6a880077e
SeungTae <dltmdxo1589@daum.net>
2024-02-28 18:16:28 +0900

    Test Data

--- src/main/java/seungtae/first/TestData.java
+++ src/main/java/seungtae/first/TestData.java
@@ -1,6 +1,25 @@
 package seungtae.first;
 
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import seungtae.first.member.Member;
+import seungtae.first.member.MemberRepository;
+
+@Component
+@RequiredArgsConstructor
 public class TestData {
 
+    private final MemberRepository memberRepository;
+
+    @PostConstruct
+    public void init() {
+        Member member = new Member();
+        member.setId("st");
+        member.setName("SeungTae");
+        member.setPassword("123!");
+
+        memberRepository.save(member);
+    }
 }
 
