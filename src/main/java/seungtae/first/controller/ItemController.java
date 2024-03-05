57488c55f7dfb7af747c9da31bbaf78fa1056c9f
SeungTae <dltmdxo1589@daum.net>
2024-03-05 21:38:55 +0900

    Product related Controller

---
+++ src/main/java/seungtae/first/controller/ItemController.java
@@ -0,0 +1,24 @@
+package seungtae.first.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/items")
+@RequiredArgsConstructor
+public class ItemController {
+
+    @GetMapping
+    public String items() {
+        return "item/items";
+    }
+
+    @GetMapping("/item")
+    public String item() {
+        return "item/item";
+    }
+}
