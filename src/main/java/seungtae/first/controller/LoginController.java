2a3bcc5747650ee030d32541cdb384c2b7020abb
SeungTae <dltmdxo1589@daum.net>
2024-03-05 21:37:33 +0900

    Change to Clean Code

--- src/main/java/seungtae/first/controller/LoginController.java
+++ src/main/java/seungtae/first/controller/LoginController.java
@@ -1,6 +1,7 @@
 package seungtae.first.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
@@ -12,21 +13,23 @@ import seungtae.first.Service.LoginService;
 import seungtae.first.member.LoginForm;
 import seungtae.first.member.Member;
 
+@Slf4j
 @Controller
-@RequestMapping
+@RequestMapping("/login")
 @RequiredArgsConstructor
 public class LoginController {
 
     private final LoginService loginService;
 
-    @GetMapping("/login")
-    public String login() {
+    @GetMapping
+    public String login(@ModelAttribute("loginForm") LoginForm loginform) {
         return "member/loginForm";
     }
 
-    @PostMapping("/login")
+    @PostMapping
     public String login(@Validated @ModelAttribute LoginForm loginForm, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
+            log.info("Login Fail!!!!");
             return "member/loginForm";
         }
 
@@ -34,9 +37,11 @@ public class LoginController {
 
         if (loginMember == null) {
             bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+            log.info("Login Fail!!!!");
             return "member/loginForm";
         }
 
+        log.info("Login Success!!! = {}", loginMember);
         return "redirect:/";
     }
 }
