8a1a8638ca3d8c3b55e4ebf71f61b689c8aa2c86
SeungTae <dltmdxo1589@daum.net>
2024-03-20 23:38:17 +0900

    Add 'Item' Object

---
+++ src/main/java/seungtae/first/item/Item.java
@@ -0,0 +1,11 @@
+package seungtae.first.item;
+
+public class Item {
+
+    private Long itemSequence;  // 리스트 고유 인덱스
+
+    private String writer;  // 작성자
+    private int date;   // 날짜
+    private String title;   // 제목
+    private String content; // 내용
+}
