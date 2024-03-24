b4468152ee4f68fabf622cfc59535fb5fbd5b9ed
SeungTae <dltmdxo1589@daum.net>
2024-03-25 00:58:57 +0900

    code fix

--- src/main/java/seungtae/first/item/Item.java
+++ src/main/java/seungtae/first/item/Item.java
@@ -3,13 +3,15 @@ package seungtae.first.item;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
 @Getter @Setter
 public class Item {
 
     private Long itemSequence;  // 리스트 고유 인덱스
-
+    private LocalDate date;   // 날짜
     private String writer;  // 작성자
-    private int date;   // 날짜
+
     private String title;   // 제목
     private String content; // 내용
 }
