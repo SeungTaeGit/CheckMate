62a53ac40b799372eabf2d7a0e825fefcf603ade
SeungTae <dltmdxo1589@daum.net>
2024-03-28 00:44:54 +0900

    Add Test data

--- src/main/java/seungtae/first/TestData.java
+++ src/main/java/seungtae/first/TestData.java
@@ -3,7 +3,9 @@ package seungtae.first;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import seungtae.first.list.List;
 import seungtae.first.member.Member;
+import seungtae.first.repository.ItemRepository;
 import seungtae.first.repository.MemberRepository;
 
 @Component
@@ -11,15 +13,14 @@ import seungtae.first.repository.MemberRepository;
 public class TestData {
 
     private final MemberRepository memberRepository;
+    private final ItemRepository itemRepository;
 
     @PostConstruct
     public void init() {
-        Member member = new Member();
-        member.setId("st");
-        member.setName("SeungTae");
-        member.setPassword("123!");
+        memberRepository.save(new Member("st", "SeungTae", "123!"));
 
-        memberRepository.save(member);
+        itemRepository.saveItem(new List("Test Title", "Test Contect"));
+        itemRepository.saveItem(new List("Test Title 2", "Test Contect 2"));
     }
 }
 
